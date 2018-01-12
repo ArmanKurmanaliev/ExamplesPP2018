e3a7d1465216c03e398f3c47e459f8b47cf8ee46
Askar K. Akshabaev <a.akshabaev@kbtu.kz>
2018-01-12 11:39:48 +0600

    class Student

--- ClassExample1/ClassExample1/Program.cs
+++ ClassExample1/ClassExample1/Program.cs
@@ -6,13 +6,36 @@ using System.Threading.Tasks;
 
 namespace ClassExample1
 {
+    class Student
+    {
+        public String name, surname;
+        public double gpa;
+
+        public Student(String name, String surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            int a = int.Parse(Console.ReadLine());
-            int b = int.Parse(Console.ReadLine());
-            Console.WriteLine(a + b);
+            List<Student> students = new List<Student>();
+            Random r = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                Student student = new Student("Student_Name" + i, "Student_surname" + i);
+                student.gpa = r.Next(2, 4) + r.NextDouble();
+                students.Add(student);
+            }
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.name + " " + student.surname + " " + student.gpa);
+            }
+            Console.ReadKey();
         }
     }
 }
