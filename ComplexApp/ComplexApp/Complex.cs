8ac5177c3d0bea51af102f5f6341b0928f03257f
Askar K. Akshabaev <a.akshabaev@kbtu.kz>
2018-01-18 13:07:36 +0600

    Complex number added

---
+++ ComplexApp/ComplexApp/Complex.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexApp
+{
+    class Complex
+    {
+        public int a, b;
+        public Complex() { }
+        public Complex(int x, int y)
+        {
+            this.a = x;
+            this.b = y;
+        }
+
+        // this->cmp1, c -> cmp2
+        public Complex Add(Complex c)
+        {
+            Complex res = new Complex(this.a + c.a, this.b + c.b);
+            // res = new Complex(6, 8);
+            // res.a = 6
+            // res.b = 8
+            return res;
+
+        }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.a + c2.a, c1.b + c2.b);
+            res.Simplify();
+            return res;
+        }
+
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.a - c2.a, c1.b - c2.b);
+            return res;
+        }
+
+        public void Simplify()
+        {
+            int _a = this.a;
+            int _b = this.b;
+
+            while (_a > 0 && _b > 0)
+            {
+                if (_a > _b)
+                    _a = _a % _b;
+                else
+                    _b = _b % _a;
+            }
+            // _a = 0, _b = 0 gcd(a, b) = _a + _b
+            int gcd = _a + _b;
+            this.a /= gcd;
+            this.b /= gcd;
+        }
+
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+    }
+}
---
+++ ComplexApp/ComplexApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex cmp1 = new Complex(5, 6);
+            Complex cmp2 = new Complex(1, 2);
+            // Complex cmp3 = cmp1.Add(cmp2); // (6, 8)
+            Complex cmp3 = cmp1 + cmp2;
+            // Complex cmp4 = cmp1 - cmp2;
+            
+
+            Console.WriteLine(cmp3);
+            Console.ReadKey();
+        }
+    }
+}
